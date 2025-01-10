56556e5be0381a316aab2bb666e251630fb01b1d
@naimish587 <naimish587@gmail.com>
2025-01-10 22:39:58 +0530

    add json data

---
+++ lib/Models/shloka_model.dart
@@ -0,0 +1,25 @@
+class Shloka {
+  final String num;
+  final String shloka;
+  final String meaning;
+  final String synonyms;
+  final String purport;
+
+  Shloka({
+    required this.num,
+    required this.shloka,
+    required this.meaning,
+    required this.synonyms,
+    required this.purport,
+  });
+
+  factory Shloka.fromJson(Map<String, dynamic> json) {
+    return Shloka(
+      num: json['num'],
+      shloka: json['shloka'],
+      meaning: json['meaning'],
+      synonyms: json['Synonyms'],
+      purport: json['Purport'],
+    );
+  }
+}
--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,39 +1,60 @@
 // ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, must_be_immutable
 
-import 'package:e_book_app/Controllers/home_controller.dart';
+import 'dart:convert';
+
 import 'package:e_book_app/Helpers/utilities.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 
 import '../Helpers/common_widgets.dart';
 
 class HomePage extends StatelessWidget {
-  HomeController _homecontroller = Get.put(HomeController());
+  Future<Map<String, dynamic>> readJson() async {
+    final String response = await rootBundle.loadString('assets/shlokas.json');
+    return json.decode(response);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        surfaceTintColor: Colors.transparent,
         backgroundColor: AppColors.primaryColor,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          surfaceTintColor: Colors.transparent,
-          backgroundColor: AppColors.primaryColor,
-          centerTitle: true,
-          title: CommonWidgets().textWidget(
-              text: "श्रीमद भगवद गीता",
-              size: 25.0,
-              weight: FontWeight.bold,
-              align: TextAlign.center),
-        ),
-        body: ListView.builder(
-          itemCount: 15,
-          shrinkWrap: true,
-          physics: ClampingScrollPhysics(),
-          itemBuilder: (context, index) {
-            return listWidget(
-              shlokname: "Naimish",
+        centerTitle: true,
+        title: CommonWidgets().textWidget(
+            text: "श्रीमद भगवद गीता",
+            size: 25.0,
+            weight: FontWeight.bold,
+            align: TextAlign.center),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: readJson(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final chapters = snapshot.data!['hi'] as Map<String, dynamic>;
+            return ListView.builder(
+              itemCount: chapters.length,
+              itemBuilder: (context, index) {
+                final key = chapters.keys.elementAt(index);
+                final value = chapters[key];
+                return ListTile(
+                  title: Text(key),
+                  subtitle: Text(value.toString()),
+                );
+              },
             );
-          },
-        ));
+          } else {
+            return const Center(child: Text("No data available."));
+          }
+        },
+      ),
+    );
   }
 
   Widget listWidget({shlokname}) {
